72048b2807c6ccfc83cb35934829299e38641d7c
Yauhen Krauchanka <yauhen.krauchanka@innowise-group.com>
2022-11-17 22:23:06 +0100

    init: created and configured the app

---
+++ emarketplace/.eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 8 },
+  ignorePatterns: ['node_modules/*', '!.prettierrc.js', 'src/types/*'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // TypeScript rules
+    'plugin:react/recommended', // React rules
+    'plugin:react-hooks/recommended', // React hooks rules
+    'plugin:jsx-a11y/recommended', // Accessibility rules#
+    'plugin:prettier/recommended', // Prettier plugin
+    'next/core-web-vitals',
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'no-unused-vars': 'off',
+    'eslint-plugin-jsx-a11y': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'react/display-name': 'off',
+    'no-console': ['warn'],
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    '@typescript-eslint/no-empty-interface': [
+      'error',
+      {
+        allowSingleExtends: false,
+      },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      { allowExpressions: true },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ emarketplace/pages/_app.tsx
@@ -0,0 +1,28 @@
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from 'app/theming/theme';
+import createEmotionCache from 'app/theming/createEmotionCache';
+import { ReactElement } from 'react';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function App(props: MyAppProps): ReactElement {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
